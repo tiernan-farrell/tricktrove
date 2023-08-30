7f0ec76779fd58116b72efd106df35cf9e85c3a1
tiernan-farrell <farrell.331@osu.edu>
2023-08-30 01:39:51 -0400

    Ignore home route

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export default authMiddleware({
   publicRoutes: ["/api/webhook/clerk", "/"],
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk",  "/api/uploadthing"],
+  ignoredRoutes: ["/api/webhook/clerk",  "/api/uploadthing", "/"],
 });
  
 export const config = {
