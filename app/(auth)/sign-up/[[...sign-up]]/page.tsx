d9dc09364e62de26c1f7905f011fbafe8bdf2a89
tiernan-farrell <farrell.331@osu.edu>
2023-09-12 22:49:05 -0400

    Messing with Signup

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignUp redirectUrl={'/onboarding'}/>;
+  return <SignUp routing='path' redirectUrl='onboarding' path='sign-up'/>;
 }
